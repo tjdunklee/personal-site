d07fd764351767abc7e01f7c95e68755585d0c11
tjdunklee <tjdunklee@gmail.com>
2019-01-28 12:07:58 -0600

    Figuring out favicon details in React helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,11 @@ import Contact from '../components/Section-contact'
 
 import '../assets/scss/application.scss'
 
+import favicon16 from '../assets/images/favicon-16x16.png'
+import favicon32 from '../assets/images/favicon-32x32.png'
+import appleTouchIcon from '../assets/images/apple-touch-icon.png'
+import safariPinnedTab from '../assets/images/safari-pinned-tab.svg'
+
 class Template extends React.Component {
   constructor(props) {
     super(props);
@@ -68,8 +73,18 @@ class Template extends React.Component {
 
     return (
       <div>
-        <Helmet defaultTitle={`Tim Dunklee - Front End Web Developer - Minneapolis MN`} titleTemplate={`%s | Tim Dunklee`}>
+        <Helmet 
+          link={[
+            { rel: 'icon', type:'image/png', sizes: '16x16', href: `${favicon16}` },
+            { rel: 'icon', type:'image/png', sizes: '32x32', href: `${favicon32}` },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: `${appleTouchIcon}` },
+            { rel: 'mask-icon', href: `${safariPinnedTab}`, color: '#333333' },
+          ]}
+          defaultTitle={`Tim Dunklee - Front End Web Developer - Minneapolis MN`}
+          titleTemplate={`%s | Tim Dunklee`}
+        >
           <meta name="description" content="The personal portfolio of front-end web developer and boardsports enthusiast Tim Dunklee." />
+          <meta name="msapplication-TileColor" content="#333333" />
           <html lang="en" class={ this.state.darkMode ? 'dark-mode' : '' } />
         </Helmet>
         <Header location={location} toggleDarkMode={this.toggleDarkMode.bind(this)} darkMode={this.state.darkMode} />
