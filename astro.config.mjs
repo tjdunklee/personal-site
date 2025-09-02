cb05ccdfeb7ae29f8e2913abfaffd9faed990af7
tjdunklee <tjdunklee@gmail.com>
2025-09-02 12:26:07 -0500

    Adds trailingSlash config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.timdunklee.com',
+  trailingSlash: 'always',
   markdown: {
     shikiConfig: {
       theme: 'github-dark'
